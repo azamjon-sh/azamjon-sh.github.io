bace8712aca4c46b547547217f43ff26c1555e2f
azamjon_sh <azam.sharpov99@mail.ru>
2020-08-28 15:16:58 +0300

    Test task

---
+++ test-task/js/script.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function($) {
+
+  /* Products block */
+  var items = $('.products__item')
+  $('.products__categories__item').click(function() {
+    if ($(this).hasClass('active')) {
+      $('.products__categories__item').removeClass('active')
+      $('.products__item').removeClass('active disable')
+    }
+    else {
+      $('.products__categories__item').removeClass('active')
+      $(this).addClass('active')
+
+      if ($(".products__categories__item.active").hasClass('front')) {
+        $('.products__item__bracket').addClass('disable');
+        $('.products__item__drow').addClass('disable');
+        $('.products__item__front').removeClass('disable');
+
+      }
+
+      else if ($(".products__categories__item.active").hasClass('brackets')) {
+        $('.products__item__front').addClass('disable');
+        $('.products__item__drow').addClass('disable');
+        $('.products__item__bracket').removeClass('disable');
+
+      }
+
+      else if ($(".products__categories__item.active").hasClass('drow')) {
+        $('.products__item__bracket').addClass('disable');
+        $('.products__item__front').addClass('disable');
+        $('.products__item__drow').removeClass('disable');
+
+      }
+    }
+  });
+
+
+  /* more products */
+
+  $('.show__more').click(function() {
+
+    if ($(this).hasClass('hide')) {
+      $('.products__item:nth-child(n+9)').slideUp();
+      $(this).removeClass('hide').text('ПОКАЗАТЬ ВСЕ')
+
+    }
+
+    else{
+      $(this).addClass('hide').text('СКРЫТЬ')
+      $('.products__item:nth-child(n+9)').slideDown();
+
+    }
+  });
+});
