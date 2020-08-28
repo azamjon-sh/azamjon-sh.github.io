a52a7b4da5a2334fc020f3d77ecedc39d6cb477d
azamjon_sh <azam.sharpov99@mail.ru>
2020-08-28 18:25:35 +0300

    contact block

--- test-task/js/script.js
+++ test-task/js/script.js
@@ -51,4 +51,17 @@ jQuery(document).ready(function($) {
 
     }
   });
+  var header = $('.header'),
+  scrollPrev = 0;
+
+  $(window).scroll(function() {
+    var scrolled = $(window).scrollTop();
+    
+    if ( scrolled > 100 && scrolled > scrollPrev ) {
+      header.addClass('out');
+    } else {
+      header.removeClass('out');
+    }
+    scrollPrev = scrolled;
+  });
 });
