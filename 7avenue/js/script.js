3caa19d626b052c1d79f88157c545941f1dca805
Azam Sharipov <azam.sharipov99@mail.ru>
2021-04-26 14:56:25 +0300

    7avenue

--- 7avenue/js/script.js
+++ 7avenue/js/script.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
-    var swiper = new Swiper('#main-slider', {
+    var rooms = new Swiper('#main-slider', {
+        effect:'fade',
         pagination: {
             el: '#main-slider .slider-pagination',
             type: 'fraction',
         },
+        longSwipesMs: 50,
         navigation: {
             nextEl: '#main-slider .arrow-next',
             prevEl: '#main-slider .arrow-prev',
+        },
+    });
+    var discounts = new Swiper('#discount', {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 32,
+        pagination: false,
+        navigation: {
+            nextEl: '.discount-prev',
+            prevEl: '.discount-next',
         },
     });
     $('.fullscreen-close').click(function () {
@@ -29,4 +41,30 @@ $(document).ready(function () {
             }
         }
     })
+    ymaps.ready(function () {
+        initMaps();
+    });
 })
+function initMaps() {
+    map = new ymaps.Map('map', {
+        center: [53.205701, 50.126650],
+        zoom: 15,
+        controls: ['zoomControl']
+    }, {
+        suppressMapOpenBlock: true,
+        yandexMapDisablePoiInteractivity: true,
+        viewportMargin: 0
+    });
+    var myPlacemark = new ymaps.Placemark([53.205701, 50.126650], {
+        hintContent: '7 Avenue Hotel & SPA',
+        balloonContent: '7 Avenue Hotel & SPA'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-object.svg',
+        iconImageSize: [50, 60],
+        iconImageOffset: [-16,-16],
+    });
+    map.geoObjects.add(myPlacemark);
+
+    map.behaviors.disable('scrollZoom')
+}
