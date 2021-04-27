041b270c7652a8e2e26c1d889f9413bc02ee8638
Azam Sharipov <azam.sharipov99@mail.ru>
2021-04-27 14:18:00 +0300

    7avenue 4

--- 7avenue/js/script.js
+++ 7avenue/js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
+    $('.preloader-content').addClass('start')
+    $('.preloader-first').slideDown(400)
+    if (window.screen.width > 1000) {
+        contentEffect(1);
+    } else {
+        contentEffect(10);
+    }
+    setTimeout(function () {
+        $('.preloader-first').addClass('to-top')
+        $('.preloader-second').fadeIn('300')
+    }, 500);
+    let timerId = setTimeout(startPreload, 2000, false)
     var rooms = new Swiper('#main-slider', {
-        effect:'fade',
+        effect: 'fade',
         pagination: {
             el: '#main-slider .slider-pagination',
             type: 'fraction',
@@ -15,11 +27,26 @@ $(document).ready(function () {
         slidesPerView: 3,
         loop: true,
         spaceBetween: 32,
-        pagination: false,
-        navigation: {
-            nextEl: '.discount-prev',
-            prevEl: '.discount-next',
+        pagination: {
+            el: '#discount-pagination',
+            type: 'bullets'
         },
+        navigation: {
+            nextEl: '.discount-next',
+            prevEl: '.discount-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            575: {
+                slidesPerView: 3,
+            },
+            768: {
+                pagination: false,
+                slidesPerView: 3,
+            },
+        }
     });
     $('.fullscreen-close').click(function () {
         $('.header-fullscreen').slideUp()
@@ -45,6 +72,13 @@ $(document).ready(function () {
         initMaps();
     });
 })
+
+function startPreload() {
+    $('body').removeClass('no-scroll')
+    $('.preloader').fadeOut()
+    $('.introduction').removeClass('scaled')
+}
+
 function initMaps() {
     map = new ymaps.Map('map', {
         center: [53.205701, 50.126650],
@@ -62,9 +96,83 @@ function initMaps() {
         iconLayout: 'default#image',
         iconImageHref: 'img/map-object.svg',
         iconImageSize: [50, 60],
-        iconImageOffset: [-16,-16],
+        iconImageOffset: [-16, -16],
     });
     map.geoObjects.add(myPlacemark);
 
     map.behaviors.disable('scrollZoom')
+}
+
+function contentEffect(val) {
+    if ($('.effecttext').length) {
+        $('.effecttext').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated fadeInUp',
+            offset: 200 / val
+        });
+    }
+    if ($('.effectfadetop').length) {
+        $('.effectfadetop').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated animate__fadeInUpBig',
+            offset: 0
+        });
+    }
+
+    if ($('.effecttextfast').length) {
+        $('.effecttextfast').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated-04s fadeInUp',
+            offset: 200 / val
+        });
+    }
+
+    if ($('.effectfade').length) {
+        $('.effectfade').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated fadeInEffect',
+            offset: 200 / val
+        });
+    }
+
+    if ($('.effectfadeleft').length) {
+        $('.effectfadeleft').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated fadeInLeft',
+            offset: 200 / val
+        });
+    }
+    if ($('.effectfaderight').length) {
+        $('.effectfaderight').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated animate__fadeInRightBig',
+            offset: 200 / val
+        });
+    }
+
+    if ($('.effecttextdown').length) {
+        $('.effecttextdown').addClass('hidden').viewportChecker({
+            classToAdd: 'visible animated animate__fadeInDownBig',
+            offset: 0 / val
+        });
+    }
+    if ($('.iphonex').length) {
+        $('.iphonex').addClass('active').viewportChecker({
+            classToAdd: 'visible animated',
+            offset: 400 / val
+        });
+    }
+
+    if ($('#index_clients').length) {
+        $('#index_clients').viewportChecker({
+            classToAdd: 'show',
+            offset: 400 / val,
+            callbackFunction: function (elem, action) {
+
+            }
+        });
+    }
+
+    if ($('#index_clients').length) {
+        $('#index_clients').viewportChecker({
+            classToAdd: 'show',
+            offset: 400 / val,
+            callbackFunction: function (elem, action) {
+            }
+        });
+    }
 }
